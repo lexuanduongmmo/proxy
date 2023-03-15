dc8837008f10bbdd5d5cd6f17e33fa3155639c6e
lexuanduongvip <lexuanduongnbhntt@gmail.com>
2023-03-15 07:13:07 -0700

    Update 3proxy.sh

--- scripts/3proxy.sh
+++ scripts/3proxy.sh
@@ -2,14 +2,14 @@ install_3proxy() {
     echo "installing 3proxy"
     mkdir -p /3proxy
     cd /3proxy
-    URL="https://github.com/z3APA3A/3proxy/archive/0.9.2.tar.gz"
+    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.2
+    cd 3proxy-0.9.3
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    mv /3proxy/3proxy-0.9.2/bin/3proxy /usr/local/etc/3proxy/bin/
-    wget https://raw.githubusercontent.com/xlandgroup/ipv4-ipv6-proxy/master/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.2/scripts/3proxy.service2
-    mv /3proxy/3proxy-0.9.2/scripts/3proxy.service2 /usr/lib/systemd/system/
+    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
+    wget https://raw.githubusercontent.com/lexuanduongvip/ipv4-ipv6-proxy-master/main/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
+    mv /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/
     systemctl daemon-reload
     systemctl enable 3proxy
     cd $WORKDIR
