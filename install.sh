f3a99a0b37375ff81118744a7901d49dca181056
Le Xuan Duong <104637023+lexuanduongvip@users.noreply.github.com>
2023-05-02 03:46:23 +0700

    Update install.sh
    Signed-off-by: Le Xuan Duong <104637023+lexuanduongvip@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,35 +1,79 @@
 #!/bin/sh
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
-    echo
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+main_interface=$(ip route get (8.8.8.8) | awk -- '{printf $5}')
 gen64() {
-    ip64() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"		
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
-    version=0.9.3
-    apt-get update && apt-get -y upgrade
-    apt-get install gcc make git -y
-    wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/3proxy/${version}.tar.gz
-    tar xzf 3proxy-${version}.tar.gz
-    cd 3proxy-${version}
+    echo "installing 3proxy"
+    mkdir -p /3proxy
+    cd /3proxy
+    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-0.9.3
     make -f Makefile.Linux
-    cd src
-    mkdir /etc/3proxy/
-    mv 3proxy /etc/3proxy/
-    cd /etc/3proxy/
-    wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
-    chmod 600 /etc/3proxy/3proxy.cfg
-    mkdir /var/log/3proxy/
-    cd /etc/init.d/
-    wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
-    chmod  +x /etc/init.d/3proxy
-    update-rc.d 3proxy defaults
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
+    wget https://raw.githubusercontent.com/lexuanduongvip/ipv4-ipv6-proxy-master/main/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
+    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
+    systemctl link /usr/lib/systemd/system/3proxy.service
+    systemctl daemon-reload
+#    systemctl enable 3proxy
+    echo "* hard nofile 999999" >>  /etc/security/limits.conf
+    echo "* soft nofile 999999" >>  /etc/security/limits.conf
+    echo "net.ipv6.conf.$main_interface.proxy_ndp=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+    echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.eth0.proxy_ndp=1" >> /etc/sysctl.conf
+   echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
+   echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
+   echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+   echo "net.ipv6.ip_nonlocal_bind=1" >> /etc/sysctl.conf
+   echo "vm.max_map_count=195120" >> /etc/sysctl.conf
+   echo "kernel.pid_max=195120" >> /etc/sysctl.conf
+   echo "net.ipv4.ip_local_port_range=1024 65000" >> /etc/sysctl.conf
+   echo -e '#!/bin/bash \n'  >> /etc/rc.local
+echo "ulimit -n 600000" >> /etc/rc.local
+echo "ulimit -u 600000" >> /etc/rc.local
+echo "ulimit -i 20000" >> /etc/rc.local
+echo "ip -6 addr add ${base_net}2 peer ${base_net}1 dev eth0" >> /etc/rc.local
+echo "sleep 5" >> /etc/rc.local
+echo "ip -6 route add default via ${base_net}1 dev eth0" >> /etc/rc.local
+echo "ip -6 route add local ${base_net}/${mask} dev lo" >> /etc/rc.local
+echo "/root/ndppd/ndppd -d -c /root/ndppd/ndppd.conf" >> /etc/rc.local
+echo -e "\nexit 0\n" >> /etc/rc.local
+echo -e '#!/bin/bash \n'  >> /etc/rc.local
+echo "ulimit -n 600000" >> /etc/rc.local
+echo "ulimit -u 600000" >> /etc/rc.local
+echo "ulimit -i 20000" >> /etc/rc.local
+echo "ip -6 addr add ${base_net}2 peer ${base_net}1 dev eth0" >> /etc/rc.local
+echo "sleep 5" >> /etc/rc.local
+echo "ip -6 route add default via ${base_net}1 dev eth0" >> /etc/rc.local
+echo "ip -6 route add local ${base_net}/${mask} dev lo" >> /etc/rc.local
+echo "/root/ndppd/ndppd -d -c /root/ndppd/ndppd.conf" >> /etc/rc.local
+echo -e "\nexit 0\n" >> /etc/rc.localecho -e '#!/bin/bash \n'  >> /etc/rc.local
+echo "ulimit -n 600000" >> /etc/rc.local
+echo "ulimit -u 600000" >> /etc/rc.local
+echo "ulimit -i 20000" >> /etc/rc.local
+echo "ip -6 addr add ${base_net1}2/64 dev eth0" >> /etc/rc.local
+echo "ip -6 route add default via ${base_net1}1" >> /etc/rc.local
+echo "ip -6 route add local ${base_net}/${mask} dev lo" >> /etc/rc.local
+echo "/root/ndppd/ndppd -d -c /root/ndppd/ndppd.conf" >> /etc/rc.local
+echo -e "\nexit 0\n" >> /etc/rc.local
+ sysctl -p
+    systemctl stop firewalld
+    systemctl disable firewalld
+
     cd $WORKDIR
 }
 
@@ -37,11 +81,15 @@ gen_3proxy() {
     cat <<EOF
 daemon
 maxconn 2000
-na
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:678:974:3::/64
+nserver 2001:678:974:2::/64
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
+stacksize 6291456 
 flush
 auth strong
 
@@ -61,33 +109,35 @@ EOF
 }
 
 upload_proxy() {
+    cd $WORKDIR
     local PASS=$(random)
     zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
-    
+    URL=$(curl -F "file=@proxy.zip" https://file.io)
+
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
     echo "Password: ${PASS}"
-    
+
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 echo "installing apps"
+yum -y install gcc net-tools bsdtar zip make >/dev/null
 
 install_3proxy
 
@@ -101,20 +151,27 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create? Example 500"
-read COUNT
-
 FIRST_PORT=10000
-LAST_PORT=11000
+LAST_PORT=12000
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
+echo NM_CONTROLLED="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
-gen_3proxy >/etc/3proxy/3proxy.cfg
-ulimit -S -n 65535
-/etc/init.d/3proxy start
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+systemctl start NetworkManager.service
+# ifup ${main_interface}
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 65535
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
+EOF
+
+bash /etc/rc.local
 
 gen_proxy_file_for_user
 
