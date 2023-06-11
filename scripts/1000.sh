7fd329b326fb52bcd408c0752e1bf4fa20af64a9
Le Xuan Duong <104637023+lexuanduongvip@users.noreply.github.com>
2023-06-11 18:27:04 +0700

    Update 1000.sh
    Signed-off-by: Le Xuan Duong <104637023+lexuanduongvip@users.noreply.github.com>

--- scripts/1000.sh
+++ scripts/1000.sh
@@ -1,16 +1,13 @@
-#!/bin/sh
 random() {
 	tr </dev/urandom -dc A-Za-z0-9 | head -c5
 	echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-main_interface=$(ip route get (8.8.8.8) | awk -- '{printf $5}')
 
 gen64() {
 	ip64() {
 		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-		if [[ "\$mask" = "64" && "\$mask" = "48" && "\$mask" = "32" ]]; then
 	}
 	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
@@ -18,14 +15,14 @@ install_3proxy() {
     echo "installing 3proxy"
     mkdir -p /3proxy
     cd /3proxy
-    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
+    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.3
+    cd 3proxy-0.9.4
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
-    wget https://raw.githubusercontent.com/lexuanduongvip/ipv4-ipv6-proxy-master/main/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
-    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
+    mkdir -p /usr/local/3proxy/{bin,logs,stat}
+    mv /3proxy/3proxy-0.9.4/bin/3proxy /usr/local/etc/3proxy/bin/
+    wget https://raw.githubusercontent.com/thuongtin/ipv4-ipv6-proxy/master/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.4/scripts/3proxy.service2
+    cp /3proxy/3proxy-0.9.4/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
     systemctl link /usr/lib/systemd/system/3proxy.service
     systemctl daemon-reload
 #    systemctl enable 3proxy
@@ -36,43 +33,10 @@ install_3proxy() {
     echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
     echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
     echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.eth0.proxy_ndp=1" >> /etc/sysctl.conf
-   echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
-   echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
-   echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-   echo "net.ipv6.ip_nonlocal_bind=1" >> /etc/sysctl.conf
-   echo "vm.max_map_count=195120" >> /etc/sysctl.conf
-   echo "kernel.pid_max=195120" >> /etc/sysctl.conf
-   echo "net.ipv4.ip_local_port_range=1024 65000" >> /etc/sysctl.conf
-   echo -e '#!/bin/bash \n'  >> /etc/rc.local
-echo "ulimit -n 600000" >> /etc/rc.local
-echo "ulimit -u 600000" >> /etc/rc.local
-echo "ulimit -i 20000" >> /etc/rc.local
-echo "ip -6 addr add ${base_net}2 peer ${base_net}1 dev eth0" >> /etc/rc.local
-echo "sleep 5" >> /etc/rc.local
-echo "ip -6 route add default via ${base_net}1 dev eth0" >> /etc/rc.local
-echo "ip -6 route add local ${base_net}/${mask} dev lo" >> /etc/rc.local
-echo "/root/ndppd/ndppd -d -c /root/ndppd/ndppd.conf" >> /etc/rc.local
-echo -e "\nexit 0\n" >> /etc/rc.local
-echo -e '#!/bin/bash \n'  >> /etc/rc.local
-echo "ulimit -n 600000" >> /etc/rc.local
-echo "ulimit -u 600000" >> /etc/rc.local
-echo "ulimit -i 20000" >> /etc/rc.local
-echo "ip -6 addr add ${base_net}2 peer ${base_net}1 dev eth0" >> /etc/rc.local
-echo "sleep 5" >> /etc/rc.local
-echo "ip -6 route add default via ${base_net}1 dev eth0" >> /etc/rc.local
-echo "ip -6 route add local ${base_net}/${mask} dev lo" >> /etc/rc.local
-echo "/root/ndppd/ndppd -d -c /root/ndppd/ndppd.conf" >> /etc/rc.local
-echo -e "\nexit 0\n" >> /etc/rc.localecho -e '#!/bin/bash \n'  >> /etc/rc.local
-echo "ulimit -n 600000" >> /etc/rc.local
-echo "ulimit -u 600000" >> /etc/rc.local
-echo "ulimit -i 20000" >> /etc/rc.local
-echo "ip -6 addr add ${base_net1}2/64 dev eth0" >> /etc/rc.local
-echo "ip -6 route add default via ${base_net1}1" >> /etc/rc.local
-echo "ip -6 route add local ${base_net}/${mask} dev lo" >> /etc/rc.local
-echo "/root/ndppd/ndppd -d -c /root/ndppd/ndppd.conf" >> /etc/rc.local
-echo -e "\nexit 0\n" >> /etc/rc.local
- sysctl -p
+    sysctl -w net.ipv4.ip_forward=1
+    sysctl -w net.ipv6.conf.all.forwarding=1
+    sysctl -w net.inet.ip.forwarding=1
+    sysctl -p
     systemctl stop firewalld
     systemctl disable firewalld
 
@@ -85,8 +49,8 @@ daemon
 maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.4.4
-nserver 2001:678:974:3::/64
-nserver 2001:678:974:2::/64
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
@@ -111,10 +75,9 @@ EOF
 }
 
 upload_proxy() {
-    cd $WORKDIR
     local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -F "file=@proxy.zip" https://file.io)
+    zip --password $PASS /home/proxy-installer/proxy.zip /home/proxy-installer/proxy.txt
+    #URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
 
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
@@ -123,7 +86,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
@@ -135,7 +98,7 @@ EOF
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig ens3 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 echo "installing apps"
@@ -148,8 +111,8 @@ WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
-IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+IPV4=$(curl -SsL ipv4.icanhazip.com)
+IPV6=$(curl -SsL ipv6.icanhazip.com) | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
