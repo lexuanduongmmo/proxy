9b6c619b53c79e56c0186a3a93cd6a01939305c6
Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>
2023-08-16 09:06:13 +0700

    Update install.sh
    Signed-off-by: Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -45,10 +45,10 @@ gen_3proxy() {
     cat <<EOF
 daemon
 maxconn 2000
-nserver 149.112.112.112
-nserver 195.46.39.39
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
+nserver 149.112.112.11
+nserver 195.46.39.40
+nserver 2606:4700:4700::1111
+nserver 2001:67c:2778::3940
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
