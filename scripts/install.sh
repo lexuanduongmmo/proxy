b4e1379c74c128dabb9f91aa8b3bae66955b1946
Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>
2023-11-10 04:21:58 +0700

    Update install.sh
    Signed-off-by: Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,13 +16,14 @@ install_3proxy() {
     echo "installing 3proxy"
     mkdir -p /3proxy
     cd /3proxy
-    URL="https://vc777.pcloud.com/dpZCxowmEZ8HxBg07Zzwf17ZZ9vEtykZ2ZZbIzZZoAcvynmmljzxMYMCwedhNLJCJRk7/3proxy-0.9.4.tar.gz"
+    URL="https://c462.pcloud.com/dpZWLylQwZP6Isg07Zzwf17ZZjmOtykZ2ZZbIzZZdJTNeB05iDQB1vJOAYcsvfbR3R1k/3proxy-0.9.3.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.4
+    cd 3proxy-0.9.3
     make -f Makefile.Linux
-    mv /3proxy/3proxy-0.9.4/bin/3proxy /usr/local/etc/3proxy/bin/
-    wget https://vc927.pcloud.com/dpZK2owmEZ2UCBg07Zzwf17ZZaiEtykZ2ZZbIzZZ2wSwKTenyvRoGQ6iNpRm0HA5VivV/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.4/scripts/3proxy.service2
-    cp /3proxy/3proxy-0.9.4/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
+    wget https://vc927.pcloud.com/dpZK2owmEZ2UCBg07Zzwf17ZZaiEtykZ2ZZbIzZZ2wSwKTenyvRoGQ6iNpRm0HA5VivV/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
+    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
     systemctl link /usr/lib/systemd/system/3proxy.service
     systemctl daemon-reload
     systemctl enable 3proxy
