d803f08a00802a5a37dfb6b742a765296526ddaa
Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>
2023-11-10 05:40:22 +0700

    Update duongmmo.sh
    Signed-off-by: Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>

--- scripts/duongmmo.sh
+++ scripts/duongmmo.sh
@@ -48,7 +48,7 @@ maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.8.8
 nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
+nserver 2606:4700:4700::1111
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
