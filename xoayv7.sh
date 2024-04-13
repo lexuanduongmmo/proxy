3c2e242e23e93d1912dab4ba7dae45b5027a8cee
Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>
2024-04-13 13:05:32 +0700

    Update xoayv7.sh
    Signed-off-by: Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>

--- xoayv7.sh
+++ xoayv7.sh
@@ -109,7 +109,7 @@ WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
-P4=$(curl -4 -s icanhazip.com)
+IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
