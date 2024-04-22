d04066ca6a75a0f2e62f3ff1c5b09045c3d39b51
Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>
2024-04-22 20:03:14 +0700

    Update test.sh
    Signed-off-by: Le Xuan Duong <104637023+lexuanduongmmo@users.noreply.github.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -54,8 +54,9 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 stacksize 6291456
+auth none
 flush
-auth strong
+
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
